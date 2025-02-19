10001270c5d3af35b2fc48e970dd55316936a861
DevNyanHa <lis131017@gmail.com>
2025-02-19 22:56:38 +0900

    Update

--- src/main/java/com/github/NaleumLibs.java
+++ src/main/java/com/github/NaleumLibs.java
@@ -3,35 +3,11 @@ package com.github;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NaleumLibs extends JavaPlugin implements Listener, NaleumLibsListener {
+public final class NaleumLibs extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
         this.getCommand("nlreload").setExecutor(new Commands());
-        NaleumEventListener.eventRegister(this);
     }
-
-    @Override
-    public void onNaleumLibsReload() {
-        System.out.println("✅ NaleumLibs가 리로드되었습니다!");
-    }
-
-    /*test
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-
-        if (event.getHand() != EquipmentSlot.HAND) return;
-
-        Player player = event.getPlayer();
-        Location location = player.getLocation().add(0, 2, 0); // 플레이어의 머리 위 2블록
-
-        Hologram test = new Hologram.Builder()
-                .location(player.getLocation().add(0, 2, 0))
-                .text("hello world")
-                .gravity(false)
-                .build();
-        test.create();
-        player.sendMessage("Hologram created at: ");
-    }*/
 }
